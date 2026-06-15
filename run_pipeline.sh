24d607375930afc3e9511a721876649984aa1a1f
James Ryan <jdr15@rice.edu>
2026-06-15 11:21:38 -0500

    Debug

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,12 +5,18 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
+# Install and verify R packages if needed
+
 # NEW: Define where data lives on THIS specific machine
 export PROJECT_DATA_DIR="/rdf/xt9/india_estimation"
 
 # Tell targets where to put the _targets store
 if [ ! -f _targets.yaml ]; then
-    Rscript -e "targets::tar_config_set(store = '${MY_PROJECT_DATA_DIR}/_targets')"
+    echo "Configuring main project data path to: $PROJECT_DATA_DIR"
+    Rscript -e "targets::tar_config_set(store = '${PROJECT_DATA_DIR}/_targets')"
 fi
 
+echo "Launching targets pipeline execution at Timestamp: $(date +'%Y-%m-%d %H:%M:%S')"
 Rscript -e "targets::tar_make()"
+
+echo "Successfully finished pipeline execution at Timestamp: $(date +'%Y-%m-%d %H:%M:%S')"
