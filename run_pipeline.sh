86b54c6c4b8cc07104fb40ca3638bf8d928d7a9b
James Ryan <jdr15@rice.edu>
2026-06-15 11:18:30 -0500

    Debug

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,7 +6,7 @@ export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 
 # NEW: Define where data lives on THIS specific machine
-export MY_PROJECT_DATA_DIR="/rdf/xt9/india_estimation"
+export PROJECT_DATA_DIR="/rdf/xt9/india_estimation"
 
 # Tell targets where to put the _targets store
 if [ ! -f _targets.yaml ]; then
